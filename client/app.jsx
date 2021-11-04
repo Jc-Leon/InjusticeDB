a7e7eadd02d709cc92250f2dac4b11d82cbfb495
Jc Leon <leonjuan123@yahoo.com>
2021-11-03 20:22:49 -0700

    completed user-can-view-existing-characters

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Home from './pages/home';
 import Header from './components/header';
-import Characters from './components/view-characters';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <>
         <Header />
         <Home />
-        <Characters />
       </>
     );
   }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <div>
+export default class Character extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/characters')
+      .then(response => response.json())
+      .then(characters => this.setState({ characters }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <>
+          <div>
       <div className="row justify-center">
         <h2>Characters</h2>
       </div>
     </div>
-  );
+
+      <div className="roster-container">
+        {this.state.characters.map(characters => (
+          <div className="cards" key={characters.name}>
+            <div className="row justify-center name">
+              <h3>{characters.name}</h3>
+            </div>
+            <div className="row justify-center cardimg">
+              <img className=" " src={characters.imageUrl} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
+</>
+    );
+
+  }
 }
